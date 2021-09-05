b8e711f82fa0ee6be890a5787db16fd7b05c8717
Kazuki Takahashi <tkhsh.schoen@gmail.com>
2021-09-05 15:59:34 +0900

    Add cpp (#1)
    * Add cpp
    
    * Add github action
    
    * Fix typo
    
    * Update yml
    
    * Update

---
+++ cpp/cpp/src/client.cpp
@@ -0,0 +1,38 @@
+#include <grpc++/grpc++.h>
+#include <iostream>
+#include <memory>
+#include <random>
+#include <string>
+#include <thread>
+#include "object.pb.h"
+#include "object.grpc.pb.h"
+
+int main(int argc, char** argv)
+{
+  const std::string addr = "server:50051";
+
+  ::grpc_samples::ObjectDetection::Stub stub(grpc::CreateChannel(addr, grpc::InsecureChannelCredentials()));
+  for (unsigned int i = 0; i < 10; i++)
+  {
+    std::cout << i << std::endl;
+    grpc::ClientContext context;
+    ::grpc_samples::ObjectRequest req;
+    ::grpc_samples::ObjectResponse res;
+    grpc::Status status = stub.GetObject(&context, req, &res);
+    if (status.ok())
+    {
+      std::cout << "id: " << res.obj().id() << std::endl;
+      std::cout << "x: " << res.obj().x() << std::endl;
+      std::cout << "y: " << res.obj().y() << std::endl;
+      std::cout << "yaw: " << res.obj().yaw() << std::endl;
+      std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+    else
+    {
+      std::cout << "gRPC failed" << std::endl;
+      return 1;
+    }
+  }
+  return 0;
+}
+
